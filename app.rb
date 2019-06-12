ae7b8b3739e7c14e8b8702b33e231ce88e1b9fbe
Andrey Grach <andrejgrach@icloud.com>
2019-06-12 22:37:13 +0300

    Create database, add gitignore

--- app.rb
+++ app.rb
@@ -3,6 +3,26 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
 
+def init_db
+  @db = SQLite3::Database.new "MyBlog.db"
+  @db.results_as_hash = true
+end
+before do
+
+  init_db
+
+end
+
+configure do
+  init_db
+  @db.execute 'create table if not exists "Posts" (id integer primary key autoincrement,
+                                      created_date date,
+                                      content text)'
+
+end
+
+
+
 get '/' do
   erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"
 end
@@ -10,4 +30,11 @@ end
 
 get '/new' do
   erb :new
+end
+
+post '/new' do
+
+content = params[:content]
+
+  erb "You typed #{content}"
 end
